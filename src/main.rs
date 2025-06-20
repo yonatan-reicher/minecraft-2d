978fc76d16614ebcbfef5d7dbe3a94bdf46e4434
Yonatan Reicher <yony252525@gmail.com>
2025-06-20 19:40:46 +0300

    Remove uneeded lines (already done somewhere else)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use minecraft_2d::*;
-use crossterm::QueueableCommand;
-use crossterm::terminal;
 
 fn main() {
-    terminal::enable_raw_mode().expect("Failed to enable raw mode");
-    std::io::stdout()
-        .queue(terminal::EnterAlternateScreen)
-        .expect("Failed to enter alternate screen");
     start_game(&mut TerminalPlatform).unwrap();
 }
